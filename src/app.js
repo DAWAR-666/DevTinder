a771616fbc00ef11ef185dace8ae8625f5a27fab
DAWAR <dawarpratham8@gmail.com>
2026-03-23 22:40:15 +0530

    created dummy admin auth midleware

--- src/app.js
+++ src/app.js
@@ -2,12 +2,23 @@ const express=require('express');
 const app=express();
 app.use('/user',(req,res,next)=>{
     console.log('Request received and authenticating at /user');
+    authToken='xyz'
     if(authToken==='xyz'){
     next()}
     else{
         res.status(401).send('Unauthorized');
     }
 })
+app.use('/admin',(req,res,next)=>{
+    console.log('Request received and authenticating at /admin');
+    authToken='abc'
+    if(authToken==='abc'){
+    next()}
+    else{
+        res.status(401).send('Unauthorized');
+    }
+})
+app.get('/admin',(req,res)=>{res.send('sent admin data')});
 app.get('/user',(req,res)=>{res.send('sent user data')});
 app.listen(8000,()=>{
     console.log('Server is running on port 8000');
