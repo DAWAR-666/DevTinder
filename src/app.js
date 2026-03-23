f188258c4831c7f4f58ba638dd9abc4243d54f26
DAWAR <dawarpratham8@gmail.com>
2026-03-23 22:38:03 +0530

    created dummy user auth middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express=require('express');
 const app=express();
-app.use((req,res)=>{res.send('Hello World')});
+app.use('/user',(req,res,next)=>{
+    console.log('Request received and authenticating at /user');
+    if(authToken==='xyz'){
+    next()}
+    else{
+        res.status(401).send('Unauthorized');
+    }
+})
+app.get('/user',(req,res)=>{res.send('sent user data')});
 app.listen(8000,()=>{
     console.log('Server is running on port 8000');
 });
