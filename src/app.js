32beec836c102259bd1cc5cdc131d5787ac9bac0
DAWAR <dawarpratham8@gmail.com>
2026-03-25 21:11:51 +0530

    created a dummy post to save document in database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const { connectdb } = require("./config/mongodb.ts");
+const User = require("./models/user.ts");
+app.post("/signup",async(req,res)=>{
+    const user=new User({
+        firstName:'pratham',
+        lastName:'dawar',
+        emailId:'qwerty@gmail.com',
+        password:'123456'
+    })
+    await user.save();
+    res.send("User created successfully");
+})
+
 connectdb()
   .then(() => {
     console.log("Connected to MongoDB");
--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -4,4 +4,3 @@ const connectdb=async()=>{
         await mongoose.connect(process.env.MONGO_URI);
 }
 module.exports={connectdb};
-export{}
