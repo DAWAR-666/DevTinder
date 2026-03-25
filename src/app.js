0bbb6c79ba313b76862542b0c88b09f9e8cef5e2
DAWAR <dawarpratham8@gmail.com>
2026-03-25 20:11:54 +0530

    used mongoose for db connection

--- src/app.js
+++ src/app.js
@@ -1,25 +1,13 @@
-const express=require('express');
-const app=express();
-app.use('/user',(req,res,next)=>{
-    console.log('Request received and authenticating at /user');
-    authToken='xyz'
-    if(authToken==='xyz'){
-    next()}
-    else{
-        res.status(401).send('Unauthorized');
-    }
-})
-app.use('/admin',(req,res,next)=>{
-    console.log('Request received and authenticating at /admin');
-    authToken='abc'
-    if(authToken==='abc'){
-    next()}
-    else{
-        res.status(401).send('Unauthorized');
-    }
-})
-app.get('/admin',(req,res)=>{res.send('sent admin data')});
-app.get('/user',(req,res)=>{res.send('sent user data')});
-app.listen(8000,()=>{
-    console.log('Server is running on port 8000');
-});
+const express = require("express");
+const app = express();
+const { connectdb } = require("./config/mongodb.ts");
+connectdb()
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(8000, () => {
+      console.log("Server is running on port 8000");
+    });
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
