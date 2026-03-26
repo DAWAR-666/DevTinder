cb5d9082ac5bce59a006e4e85e8c987caff8cfe9
DAWAR <dawarpratham8@gmail.com>
2026-03-26 14:49:38 +0530

    stored dynamic input user document in database

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
-const { connectdb } = require("./config/mongodb.ts");
-const User = require("./models/user.ts");
+const { connectdb } = require("./config/mongodb.js");
+const User = require("./models/user.js");
+app.use(express.json());
 app.post("/signup",async(req,res)=>{
-    const user=new User({
-        firstName:'pratham',
-        lastName:'dawar',
-        emailId:'qwerty@gmail.com',
-        password:'123456'
-    })
-    await user.save();
-    res.send("User created successfully");
+    const user=new User(req.body);
+    try{
+        await user.save();
+        res.send("User created successfully");
+    }catch(err){
+        return res.status(500).send("Error creating user");
+    }
 })
 
 connectdb()
