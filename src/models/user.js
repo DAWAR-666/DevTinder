3ce46ef550f7b7828e749f121736465df943b85f
DAWAR <dawarpratham8@gmail.com>
2026-03-27 18:57:11 +0530

    added email validaotr in schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-
+const validator=require('validator');
 const userSchema=new mongoose.Schema({
     firstName:{
         type:String,
@@ -15,6 +15,11 @@ const userSchema=new mongoose.Schema({
         unique:true,
         lowercase:true,
         trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Invalid email address');
+            }
+        }
     },
     password:{
         type:String,
